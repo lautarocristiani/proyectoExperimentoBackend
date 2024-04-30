f6b841da69c236904ca1916a1b95433c1f50b63a
Lautaro <lautarocristiani2001@gmail.com>
2024-04-30 19:44:59 -0300

    asd3

--- src/app.js
+++ src/app.js
@@ -35,3 +35,10 @@ app.get("/usuarios", async (req, res) => {
     const resultado = await pool.query(consulta)
     res.json(resultado[0])
 })
+
+app.post("/usuarios", async (req, res) => {
+    const { nombre, apellido, email } = req.body;
+    const consulta = `INSERT INTO "usuarios" ('${nombre}', '${apellido}', '${email}') VALUES ({}, ?, ?)`
+    const resultado = await pool.query(consulta)
+    res.json(resultado)
+})
