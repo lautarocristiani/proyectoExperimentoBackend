ac7eba7f1c7a95f7600fcd6787f9b4a76647bc7c
Lautaro <lautarocristiani2001@gmail.com>
2024-04-30 14:33:18 -0300

    a

--- src/app.js
+++ src/app.js
@@ -17,15 +17,15 @@ pool.getConnection()
         console.log('Conexión exitosa a la base de datos MySQL')
         const consulta = 'SELECT * FROM `usuarios`'
         const resultado = await pool.query(consulta)
-        console.log(resultado);
+        console.log(resultado[0]);
         connection.release() // No olvides liberar la conexión
     })
     .catch(err => {
         console.error('Error al conectar a la base de datos:', err)
     });
 
-app.listen(process.env.DB_PORT, "0.0.0.0",  () => {
-    console.log(`Servidor iniciado en el puerto ${process.env.DB_PORT}`)
+app.listen(process.env.PORT, "0.0.0.0",  () => {
+    console.log(`Servidor iniciado en el puerto ${process.env.PORT}`)
 });
 
 // Rutas API
--- src/db.js
+++ src/db.js
@@ -5,5 +5,5 @@ export const pool = await createPool({
     user: process.env.DB_USER,  
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3000
 })
