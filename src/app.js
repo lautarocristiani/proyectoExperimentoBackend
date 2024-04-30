d71b11382c23bcfacbab544dd4385dda3736af54
Lautaro <lautarocristiani2001@gmail.com>
2024-04-30 14:38:43 -0300

    anashe

--- src/app.js
+++ src/app.js
@@ -15,17 +15,15 @@ app.use(express.json())
 pool.getConnection()
     .then(async connection => {
         console.log('Conexión exitosa a la base de datos MySQL')
-        const consulta = 'SELECT * FROM `usuarios`'
-        const resultado = await pool.query(consulta)
-        console.log(resultado[0]);
         connection.release() // No olvides liberar la conexión
     })
     .catch(err => {
         console.error('Error al conectar a la base de datos:', err)
     });
 
-app.listen(process.env.PORT, "0.0.0.0",  () => {
-    console.log(`Servidor iniciado en el puerto ${process.env.PORT}`)
+app.listen(process.env.DB_PORT, "0.0.0.0",  () => {
+    console.log(`Servidor iniciado en el puerto ${process.env.DB_PORT}`)
+    console.log("PORT solo: ", PORT);
 });
 
 // Rutas API
