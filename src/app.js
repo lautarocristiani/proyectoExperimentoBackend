75cdb235d97fa2b6f2589f45b37906ecc085236c
Lautaro <lautarocristiani2001@gmail.com>
2024-05-03 11:19:22 -0300

    agregando log para ver el resultado de la consulta para eliminar

--- src/app.js
+++ src/app.js
@@ -75,6 +75,7 @@ app.delete("/usuarios/:id", async (req, res) => {
 
     try {
         const resultado = await pool.query(consulta, [id]);
+        console.log(resultado);
         if (resultado.affectedRows > 0) {
             res.status(200).json({ message: "Usuario eliminado correctamente" });
         } else {
