f1e9b410a106fcf738d0cb44a56a4ea793fded98
Lautaro <lautarocristiani2001@gmail.com>
2024-05-18 19:04:20 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import express from "express"
-import cors from "cors"
-import { pool } from "./db.js"
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
 import axios from 'axios'; 
 import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
-const app = express()
+const app = express();
 
 // Habilitamos CORS
 const corsOptions = {
@@ -23,33 +25,34 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware para parsear JSON entrante
-app.use(express.json())
+app.use(express.json());
+
+const secretKey = process.env.JWT_SECRET || 'mysecretkey';
 
 // Conexión a la base de datos
 pool.getConnection()
     .then(async connection => {
-        console.log('Conexión exitosa a la base de datos MySQL')
-        connection.release() // No olvides liberar la conexión
+        console.log('Conexión exitosa a la base de datos MySQL');
+        connection.release(); // No olvides liberar la conexión
     })
     .catch(err => {
-        console.error('Error al conectar a la base de datos:', err)
+        console.error('Error al conectar a la base de datos:', err);
     });
 
 app.listen(process.env.PORT || 4000, "0.0.0.0",  () => {
-    console.log(`Servidor iniciado en el puerto ${process.env.PORT || 4000}`)
+    console.log(`Servidor iniciado en el puerto ${process.env.PORT || 4000}`);
 });
 
 // Rutas API Usuarios
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
+    res.send("Hello World!");
+});
 
 app.get("/usuarios", async (req, res) => {
-    const consulta = 'SELECT * FROM `usuarios`'
-    const resultado = await pool.query(consulta)
-    res.json(resultado[0])
-})
+    const consulta = 'SELECT * FROM `usuarios`';
+    const resultado = await pool.query(consulta);
+    res.json(resultado[0]);
+});
 
 app.post("/usuarios", async (req, res) => {
     const { nombre, apellido, email } = req.body;
@@ -68,7 +71,6 @@ app.post("/usuarios", async (req, res) => {
     }
 });
 
-
 app.delete("/usuarios/:id", async (req, res) => {
     const { id } = req.params;
     const consulta = `DELETE FROM usuarios WHERE id = ?`;
@@ -77,14 +79,11 @@ app.delete("/usuarios/:id", async (req, res) => {
         console.log(resultado);
         if (resultado.affectedRows  > 0) {
             res.status(200).json({ message: "Usuario eliminado correctamente" });
-            alert("Usuario eliminado correctamente");
         } else {
             res.status(404).json({ message: "Usuario no encontrado" });
-            alert("Usuario no encontrado");
         }
     } catch (error) {
         res.status(500).json({ error: error.message });
-        alert("Error al eliminar usuario");
     }
 });
 
@@ -104,7 +103,6 @@ app.put("/usuarios/:id", async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
 
 // Variable global para almacenar el token
 let globalToken = null;
@@ -184,7 +182,7 @@ function processTrades(trades, interval) {
     return Object.values(groupedTrades);
 }
 
-//Del día
+// Del día
 app.get("/getTodayTrades", async (req, res) => {
     const today = new Date().toLocaleDateString('en-CA', { timeZone: 'America/Argentina/Buenos_Aires' });
     const { symbol } = req.query;
@@ -212,5 +210,59 @@ app.get("/getMarketData", async (req, res) => {
     } catch (error) {
         console.error('Error fetching Market data:', error);
         res.status(500).json({ error: error.message });
+    }
+});
+
+// Rutas de Autenticación
+app.post("/signup", async (req, res) => {
+    const { nombre, apellido, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const consultaInsertar = `INSERT INTO usuarios (nombre, apellido, email, password) VALUES (?, ?, ?, ?)`;
+
+    try {
+        await pool.query(consultaInsertar, [nombre, apellido, email, hashedPassword]);
+        res.status(201).json({ message: "Usuario registrado correctamente" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const consulta = `SELECT * FROM usuarios WHERE email = ?`;
+
+    try {
+        const [result] = await pool.query(consulta, [email]);
+        const user = result[0];
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+            res.status(200).json({ 
+                token,
+                nombre: user.nombre,
+                apellido: user.apellido
+            });
+        } else {
+            res.status(401).json({ message: "Credenciales incorrectas" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get("/protected", (req, res) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token no proporcionado" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded;
+        res.status(200).json({ message: "Acceso autorizado" });
+    } catch (error) {
+        res.status(401).json({ message: "Token inválido" });
     }
 });
